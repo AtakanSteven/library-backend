7adab078330def119d2894adab1616a05c095ad7
Atakan Beddall <atakan.beddall@sigmatelecom.com>
2022-04-09 19:07:35 +0300

    Added email sending service - nodemailer

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { ProfileModule } from "./profile/profile.module";
 import { ConfigModule } from "@nestjs/config";
 import getDatabaseUrl from "./config/database.url";
+import {EmailModule} from "./mail/email.module";
+import {BookModule} from "./book/book.module";
 
 @Module({
   imports: [
       ConfigModule.forRoot({ isGlobal: true }),
       MongooseModule.forRoot(getDatabaseUrl()),
-      ProfileModule
+      ProfileModule,
+      EmailModule,
+      BookModule
   ],
   controllers: [],
   providers: [],
