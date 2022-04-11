e1ce787bc36975b018d8f1d42b00f794ad1b32d3
Atakan Beddall <atakan.beddall@sigmatelecom.com>
2022-04-11 10:05:18 +0300

    Removed unnecessary code.

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -4,17 +4,11 @@ import {MongooseModule} from "@nestjs/mongoose";
 import {ProfileController} from "./profile.controller";
 import {ProfileService} from "./profile.service";
 import {EmailModule} from "../mail/email.module";
-import {JwtModule} from "@nestjs/jwt";
-import {jwtConstants} from "../auth/constants";
 
 @Module({
     providers: [ProfileService],
     controllers: [ProfileController],
-    imports: [MongooseModule.forFeature([{ name: Profile.name, schema: ProfileSchema },]), EmailModule,
-        JwtModule.register({
-            secret: 'secret',
-            signOptions: {expiresIn: '1000s'}
-        })],
+    imports: [MongooseModule.forFeature([{ name: Profile.name, schema: ProfileSchema },]), EmailModule],
     exports: [ProfileService]
 })
 export class ProfileModule {}
